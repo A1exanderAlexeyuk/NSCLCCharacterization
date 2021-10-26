f426f87d7215184f395bedb9bc698e031e39eb43
alexander alexeuyk <87428750+A1exanderAlexeyuk@users.noreply.github.com>
2021-10-26 12:31:51 +0300

    initial commit

---
+++ R/GenerateRegimensStats.R
@@ -0,0 +1,25 @@
+#' export
+
+generateRegimensStats <- function(connection, 
+                             cohortDatabaseSchema, 
+                             regimenIngredientTable, 
+                             cohortTable,
+                             targetIds, 
+                             linesOfTreatment, 
+                             #databaseId, 
+                             #packageName
+                             ){
+  sqlFileName <- "linesOfT.sql"
+  pathToSql <- system.file("sql", "sql_server", sqlFileName, package = packageName)
+  sql <- readChar(pathToSql, file.info(pathToSql)$size)
+      sql_tmp <- SqlRender::render(sql, 
+                                   cohort_database_schema = cohortDatabaseSchema,
+                                   cohortTable = cohortTable, 
+                                   linesOfTreatment = linesOfTreatment, 
+                                   targetIds = targetIds)
+      sql_tmp <- SqlRender::translate(sql = sql_tmp, 
+                                      targetDialect = connection@dbms)
+      
+      stats_raw <- DatabaseConnector::querySql(connection = connection, 
+                                              sql = sql_tmp, 
+                                              snakeCaseToCamelCase = T)
