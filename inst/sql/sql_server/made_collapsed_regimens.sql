0fa5eb8096edb00b7d78a0ba27fb12167273b60f
alexander alexeuyk <87428750+A1exanderAlexeyuk@users.noreply.github.com>
2021-10-27 12:04:20 +0300

    naming fix

--- inst/sql/sql_server/made_collapsed_regimens.sql
+++ inst/sql/sql_server/made_collapsed_regimens.sql
@@ -1,4 +1,4 @@
-with ctt as (select r1.person_id, r1.regimen_start_date, r1.regimen_end_date,  r1.regimen,
+with temp_1 as (select r1.person_id, r1.regimen_start_date, r1.regimen_end_date,  r1.regimen,
 			  min(r2.regimen_start_date) as regimen_start_date_new,
 			  max(r2.regimen_end_date) as regimen_end_date_new
 			  from regimeningredienttable r1
@@ -8,31 +8,31 @@ with ctt as (select r1.person_id, r1.regimen_start_date, r1.regimen_end_date,  r
 			 r1.regimen_end_date
 			  group by r1.person_id, r1.regimen_start_date, r1.regimen_end_date, r1.regimen
 			  order by 1,2,3,4),
-cte as (select *, 
+			  
+temp_2 as (select *, 
 coalesce(lag(regimen, 1) over (order by person_id) != regimen, TRUE) as New_regimen
-from ctt),
+from temp_1),
 
-sss as (select cte.person_id, cte.regimen_start_date, cte.regimen_end_date, cte.regimen, 
-coalesce(lag(cte.regimen, 1) over (order by cte.person_id) != cte.regimen, TRUE) as New_regimen,
-ctt.regimen_start_date_new  ,ctt.regimen_end_date_new 
---row_number() over (PARTITION BY  New_regimen,person_id
---Order by person_id, regimen_start_date) as Line_of_therapy  f
-from cte   join ctt
-ON cte.person_id = ctt.person_id 
-AND cte.regimen = ctt.regimen 
-AND cte.regimen_start_date = ctt.regimen_start_date_new 
-AND cte.regimen_end_date <= ctt.regimen_end_date_new 
+temp_3 as (select temp_2.person_id, temp_2.regimen_start_date, temp_2.regimen_end_date, temp_2.regimen, 
+coalesce(lag(temp_2.regimen, 1) over (order by temp_2.person_id) != temp_2.regimen, TRUE) as New_regimen,
+temp_1.regimen_start_date_new  ,temp_1.regimen_end_date_new 
+from temp_2   join temp_1
+ON temp_2.person_id = temp_1.person_id 
+AND temp_2.regimen = temp_1.regimen 
+AND temp_2.regimen_start_date = temp_1.regimen_start_date_new 
+AND temp_2.regimen_end_date <= temp_1.regimen_end_date_new 
 order by 1,2,3),
-sss1 as (
+temp_31 as (
 select person_id, regimen, regimen_start_date_new as regimen_start_date, 
 regimen_end_date_new as regimen_end_date,
 row_number() over (PARTITION BY  person_id
 Order by person_id, regimen_start_date) as Line_of_therapy 
-from sss where New_regimen != false)
+from temp_3 where New_regimen != false)
 
 select *, 
 	case when Line_of_therapy != 1 then 
 	abs(coalesce(lag(regimen_end_date, 1) over (order by person_id, regimen))
 	                                                     - regimen_start_date) 
 	else -1 end as interval_beetwen_lines
-from sss1 order by 5
+from temp_31 order by 5
+
