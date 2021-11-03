94e544d15073ccebfd7848f91e91fbe77cba203c
alexander alexeuyk <87428750+A1exanderAlexeyuk@users.noreply.github.com>
2021-11-03 13:32:08 +0300

    put only required stats(median, IQR)

--- inst/sql/sql_server/TreatmentAnalysis/TimeToTreatmentInitiation.sql
+++ inst/sql/sql_server/TreatmentAnalysis/TimeToTreatmentInitiation.sql
@@ -19,6 +19,7 @@ with init_data AS  (SELECT t.subject_id,
                          SUM(1) OVER (PARTITION BY cohort_definition_id) AS total
                   FROM init_data
                   ),
+
      quartiles AS (
                   SELECT cohort_definition_id,
                          value,
@@ -39,13 +40,9 @@ with init_data AS  (SELECT t.subject_id,
                                      AND row_number <= (CEIL(total / 2.0) + (FLOOR(total / 2.0) / 2.0) + 1)
                                      THEN value / 1.0
                              END
-                             ) OVER (PARTITION BY cohort_definition_id) AS q3,
+                             ) OVER (PARTITION BY cohort_definition_id) AS q3
 
-			power((value - avg(value) OVER (PARTITION BY cohort_definition_id
-			)), 2) / (total -1)
-		 						as Variance,
-		 	(avg(value) OVER (PARTITION BY cohort_definition_id
-			)) as mean
+
 
 
                   FROM details
@@ -54,15 +51,14 @@ with init_data AS  (SELECT t.subject_id,
 
 
 SELECT
-
-      ROUND (AVG(q3) - AVG(q1),1) AS IQR,
+       cohort_definition_id,
+       ROUND (AVG(q3) - AVG(q1),1) AS IQR,
        ROUND (MIN(value),1) AS minimum,
        ROUND (AVG(q1),1) AS q1,
        ROUND (AVG(median),1) AS median,
        ROUND (AVG(q3),1) AS q3,
-       ROUND (MAX(value),1) AS maximum,
-       ROUND (AVG(mean) ,1) as Mean,
-       ROUND (sqrt(SUM(Variance)), 1) as StD
+       ROUND (MAX(value),1) AS maximum
        'TimeToTreatmentInitiation' AS analysis_name
+
 FROM quartiles
 GROUP BY 1;
