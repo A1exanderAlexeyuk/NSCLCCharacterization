05e9e2d2f9533b641532b9f2126857ccfb56d6b3
alexander alexeuyk <87428750+A1exanderAlexeyuk@users.noreply.github.com>
2021-11-08 14:12:47 +0300

    km file tested

--- tests/testthat/test - CohortConstruction.R
+++ tests/testthat/test - CohortConstruction.R
@@ -7,7 +7,7 @@ cdmDatabaseSchema <- Sys.getenv("testcdmDatabaseSchema")
 cohortDatabaseSchema <- resultsDatabaseSchema
 cohortTable <- Sys.getenv("testcohortTable")
 databaseId <- "testDatabaseId"
-packageName = "NSCLCCharacterization"
+packageName <- "NSCLCCharacterization"
 # connectionDetails <- createConnectionDetails(dbms = "postgresql",
 #                                              server = Sys.getenv("testserver"),
 #                                              user = Sys.getenv("testuser"),
@@ -15,49 +15,62 @@ packageName = "NSCLCCharacterization"
 #                                              port = Sys.getenv("testport"))
 # conn <- connect(connectionDetails=connectionDetails)
 test_that("Create Cohort Table", {
-  connectionDetails <- createConnectionDetails(dbms = "postgresql",
-                                               server = Sys.getenv("testserver"),
-                                               user = Sys.getenv("testuser"),
-                                               password = Sys.getenv("testuser"),
-                                               port = Sys.getenv("testport"))
-  conn <- connect(connectionDetails=connectionDetails)
-  expect_error(renderTranslateQuerySql(connection = conn, sql = "INSERT INTO
+  connectionDetails <- createConnectionDetails(
+    dbms = "postgresql",
+    server = Sys.getenv("testserver"),
+    user = Sys.getenv("testuser"),
+    password = Sys.getenv("testuser"),
+    port = Sys.getenv("testport")
+  )
+  conn <- connect(connectionDetails = connectionDetails)
+  expect_error(renderTranslateQuerySql(
+    connection = conn, sql = "INSERT INTO
                                        @cohortDatabaseSchema.@cohortTable
                                        VALUES (1, 1, 1, '2000-01-01', '2000-01-01'",
-                                       cohortDatabaseSchema = cohortDatabaseSchema,
-                                       cohortTable = cohortTable))
+    cohortDatabaseSchema = cohortDatabaseSchema,
+    cohortTable = cohortTable
+  ))
 
-  NSCLCCharacterization::createCohortTable(connectionDetails = connectionDetails,
-                                          connection = conn,
-                                          cohortDatabaseSchema = cohortDatabaseSchema,
-                                          cohortTable = cohortTable,
-                                          createInclusionStatsTables = FALSE,
-                                          resultsDatabaseSchema = cohortDatabaseSchema,
-                                          cohortInclusionTable = paste0(cohortTable, "_inclusion"),
-                                          cohortInclusionResultTable = paste0(cohortTable, "_inclusion_result"),
-                                          cohortInclusionStatsTable = paste0(cohortTable, "_inclusion_stats"),
-                                          cohortSummaryStatsTable = paste0(cohortTable, "_summary_stats"))
+  NSCLCCharacterization::createCohortTable(
+    connectionDetails = connectionDetails,
+    connection = conn,
+    cohortDatabaseSchema = cohortDatabaseSchema,
+    cohortTable = cohortTable,
+    createInclusionStatsTables = FALSE,
+    resultsDatabaseSchema = cohortDatabaseSchema,
+    cohortInclusionTable = paste0(cohortTable, "_inclusion"),
+    cohortInclusionResultTable = paste0(cohortTable, "_inclusion_result"),
+    cohortInclusionStatsTable = paste0(cohortTable, "_inclusion_stats"),
+    cohortSummaryStatsTable = paste0(cohortTable, "_summary_stats")
+  )
 
-  expect_error(renderTranslateQuerySql(connection = conn, sql = "INSERT INTO
+  expect_error(renderTranslateQuerySql(
+    connection = conn, sql = "INSERT INTO
                                        @cohortDatabaseSchema.@cohortTable
                                        VALUES (1, 1, 1, '2000-01-01', '2000-01-01'",
-                                       cohortDatabaseSchema = cohortDatabaseSchema,
-                                       cohortTable = cohortTable), NA)
+    cohortDatabaseSchema = cohortDatabaseSchema,
+    cohortTable = cohortTable
+  ), NA)
 
-  expect_type(renderTranslateQuerySql(connection = conn, sql = "SELECT *
+  expect_type(renderTranslateQuerySql(
+    connection = conn, sql = "SELECT *
                                       FROM @cohortDatabaseSchema.@cohortTable",
-                                      cohortDatabaseSchema = cohortDatabaseSchema,
-                                      cohortTable = cohortTable), "list")
+    cohortDatabaseSchema = cohortDatabaseSchema,
+    cohortTable = cohortTable
+  ), "list")
 
-  renderTranslateQuerySql(connection = conn, sql = "DROP TABLE
+  renderTranslateQuerySql(
+    connection = conn, sql = "DROP TABLE
                                        @cohortDatabaseSchema.@cohortTable",
-                                       cohortDatabaseSchema = cohortDatabaseSchema,
-                                       cohortTable = cohortTable)
+    cohortDatabaseSchema = cohortDatabaseSchema,
+    cohortTable = cohortTable
+  )
 
-  expect_error(renderTranslateQuerySql(connection = conn, sql = "INSERT INTO
+  expect_error(renderTranslateQuerySql(
+    connection = conn, sql = "INSERT INTO
                                        @cohortDatabaseSchema.@cohortTable
                                        VALUES (1, 1, 1, '2000-01-01', '2000-01-01'",
-                                       cohortDatabaseSchema = cohortDatabaseSchema,
-                                       cohortTable = cohortTable))
-
+    cohortDatabaseSchema = cohortDatabaseSchema,
+    cohortTable = cohortTable
+  ))
 })
--- tests/testthat/test - CreateRegimenStatisticsTable.R
+++ tests/testthat/test - CreateRegimenStatisticsTable.R
@@ -7,31 +7,44 @@ cdmDatabaseSchema <- Sys.getenv("testcdmDatabaseSchema")
 cohortDatabaseSchema <- resultsDatabaseSchema
 cohortTable <- Sys.getenv("testcohortTable")
 databaseId <- "testDatabaseId"
-packageName = "NSCLCCharacterization"
-regimenStatsTable = "regimenStatsTable"
-regimenIngredientsTable = "cancer_regimen_ingredients_table"
+packageName <- "NSCLCCharacterization"
+regimenStatsTable <- "regimenStatsTable"
+regimenIngredientsTable <- "cancer_regimen_ingredients_table"
 test_that("Create Regimen Statistics", {
-  connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = "postgresql",
-                                                                  server = Sys.getenv("testserver"),
-                                                                  user = Sys.getenv("testuser"),
-                                                                  password = Sys.getenv("testuser"),
-                                                                  port = Sys.getenv("testport"))
-  conn <- connect(connectionDetails=connectionDetails)
-  expect_error(NSCLCCharacterization::createRegimenStats(connection = conn,
-                                                         cdmDatabaseSchema = cdmDatabaseSchema,
-                                                         cohortDatabaseSchema = cohortDatabaseSchema,
-                                                         cohortTable = cohortTable,
-                                                         regimenStatsTable = regimenStatsTable,
-                                                         regimenIngredientsTable = regimenIngredientsTable,
-                                                         gapBetweenTreatment = 120), NA)
+  connectionDetails <- DatabaseConnector::createConnectionDetails(
+    dbms = "postgresql",
+    server = Sys.getenv("testserver"),
+    user = Sys.getenv("testuser"),
+    password = Sys.getenv("testuser"),
+    port = Sys.getenv("testport")
+  )
+  conn <- connect(connectionDetails = connectionDetails)
+  expect_error(NSCLCCharacterization::createRegimenStats(
+    connection = conn,
+    cdmDatabaseSchema = cdmDatabaseSchema,
+    cohortDatabaseSchema = cohortDatabaseSchema,
+    cohortTable = cohortTable,
+    regimenStatsTable = regimenStatsTable,
+    regimenIngredientsTable = regimenIngredientsTable,
+    gapBetweenTreatment = 120
+  ), NA)
 
-  expect_true(isTRUE(unlist(DatabaseConnector::renderTranslateQuerySql(connection = conn,
-                                                         sql = "SELECT EXISTS (
+  expect_true(isTRUE(unlist(DatabaseConnector::renderTranslateQuerySql(
+    connection = conn,
+    sql = "SELECT EXISTS (
                                      SELECT FROM information_schema.tables
                                      WHERE  table_schema = @cohortDatabaseSchema
                                      AND    table_name   = @regimenStatsTable
-                                     );"))))
+                                     );", cohortDatabaseSchema = cohortDatabaseSchema,
+    regimenStatsTable = regimenStatsTable
+  ))))
 
 
+  expect_error(DatabaseConnector::renderTranslateQuerySql(
+    connection = conn,
+    sql = "DROP TABLE
+                                                          @cohortDatabaseSchema.@regimenStatsTable);",
+    cohortDatabaseSchema = cohortDatabaseSchema,
+    regimenStatsTable = regimenStatsTable
+  ), NA)
 })
-
