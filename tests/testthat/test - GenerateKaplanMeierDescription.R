7ea8851fd69865268734f6a440e17bbf0b9f54b9
alexander alexeuyk <87428750+A1exanderAlexeyuk@users.noreply.github.com>
2021-11-19 12:16:45 +0300

    fix funcs tests

--- tests/testthat/test - GenerateKaplanMeierDescription.R
+++ tests/testthat/test - GenerateKaplanMeierDescription.R
@@ -29,24 +29,16 @@ test_that("Survival test", {
     packageName = "NSCLCCharacterization",
     databaseId = databaseId
   ), "data.frame")
-  expect_error(generateSurvival(
-    connection = conn,
-    cohortDatabaseSchema = cohortDatabaseSchema,
-    cohortTable = NULL,
-    targetIds = targetIds,
-    outcomeId = outcomeId,
-    packageName = "NSCLCCharacterization",
-    databaseId = databaseId
-  ))
+
 })
 
 
 
 # !!Test passed
-test_that("generateKaplanMeierDescriptionTFITTD", {
+test_that("generateKaplanMeierDescriptionTFI", {
   # locally
   cohortDatabaseSchema <- "regimen_stats_schema"
-  regimenStatsTable <- "rstF2"
+  regimenStatsTable <- "rstF3"
   targetIds <- 1
   databaseId <- "test"
   connectionDetails <- DatabaseConnector::createConnectionDetails(
@@ -59,24 +51,53 @@ test_that("generateKaplanMeierDescriptionTFITTD", {
     pathToDriver = Sys.getenv("DATABASECONNECTOR_JAR_FOLDER")
   )
   conn <- connect(connectionDetails = connectionDetails)
-
-  expect_s3_class(NSCLCCharacterization::generateKaplanMeierDescriptionTFITTD(
+  t <-  NSCLCCharacterization::generateKaplanMeierDescriptionTFI(
     connection = conn,
     cohortDatabaseSchema = cohortDatabaseSchema,
     regimenStatsTable,
     targetIds = targetIds,
     databaseId = databaseId
-  ), "data.frame")
+  )
+  expect_s3_class(t, "data.frame")
+  expect_true(dim(t)[[1]] > 0)
 })
 
 
+# !!Test passed
+test_that("generateKaplanMeierDescriptionTFI", {
+  # locally
+  cohortDatabaseSchema <- "regimen_stats_schema"
+  regimenStatsTable <- "rstF3"
+  targetIds <- 1
+  databaseId <- "test"
+  connectionDetails <- DatabaseConnector::createConnectionDetails(
+    dbms = "postgresql",
+    server = "postgres/localhost",
+    port = "5432",
+    connectionString = "jdbc:postgresql://localhost:5432/postgres",
+    user = "postgres",
+    password = "sql",
+    pathToDriver = Sys.getenv("DATABASECONNECTOR_JAR_FOLDER")
+  )
+  conn <- connect(connectionDetails = connectionDetails)
+  t <- NSCLCCharacterization::generateKaplanMeierDescriptionTTD(
+    connection = conn,
+    cohortDatabaseSchema = cohortDatabaseSchema,
+    regimenStatsTable,
+    targetIds = targetIds,
+    databaseId = databaseId)
+  expect_s3_class(
+  t, "data.frame")
+
+  expect_true(dim(t)[[1]] > 0)
+})
 
 
 
 # !!!Test passed
 test_that("generateKaplanMeierDescriptionTNT", {
   cohortDatabaseSchema <- "regimen_stats_schema"
-  regimenStatsTable <- "rstF2"
+  regimenStatsTable <- "rstF3"
   targetIds <- 1
   databaseId <- "test"
   connectionDetails <- DatabaseConnector::createConnectionDetails(
@@ -89,12 +110,13 @@ test_that("generateKaplanMeierDescriptionTNT", {
     pathToDriver = Sys.getenv("DATABASECONNECTOR_JAR_FOLDER")
   )
   conn <- connect(connectionDetails = connectionDetails)
-
-  testthat::expect_s3_class(NSCLCCharacterization::generateKaplanMeierDescriptionTNT(
+  t <- NSCLCCharacterization::generateKaplanMeierDescriptionTNT(
     connection = conn,
     cohortDatabaseSchema = cohortDatabaseSchema,
     regimenStatsTable = regimenStatsTable,
     targetIds = targetIds,
     databaseId = databaseId
-  ), "data.frame")
+  )
+  testthat::expect_s3_class(t, "data.frame")
+  expect_true(dim(t)[[1]]>0)
 })
