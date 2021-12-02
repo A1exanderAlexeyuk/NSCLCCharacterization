2d3cb1b0bdfa3618ec946f80c694b5c2ee7ba148
alexander alexeuyk <87428750+A1exanderAlexeyuk@users.noreply.github.com>
2021-12-02 13:20:38 +0300

    initial commit

---
+++ tests/testthat/test - RunStudy.R
@@ -0,0 +1,43 @@
+library(testthat)
+library(DatabaseConnector)
+library(SqlRender)
+library(lubridate)
+resultsDatabaseSchema <- "alex_alexeyuk_results"
+cdmDatabaseSchema <- "cdm_531"
+cohortDatabaseSchema <- "alex_alexeyuk_results"
+cohortTable <- "union_table2"
+databaseId <- "testDatabaseId"
+packageName <- "NSCLCCharacterization"
+outputFolder <- getwd()
+###########################Test passed ############################
+test_that("Run Study!!!", {
+  connectionDetails <- createConnectionDetails(
+    dbms = "postgresql",
+    server = "testnode.arachnenetwork.com/synpuf_110k",
+    user = Sys.getenv("ohdsi_password"),
+    password = Sys.getenv("ohdsi_password"),
+    port = "5441"
+  )
+  conn <- connect(connectionDetails = connectionDetails)
+
+  expect_error(NSCLCCharacterization::runStudy(connectionDetails = connectionDetails,
+                                               connection = conn,
+                                               cdmDatabaseSchema = cdmDatabaseSchema,
+                                               writeDatabaseSchema = cohortDatabaseSchema,
+                                               tempEmulationSchema = NULL,
+                                               cohortDatabaseSchema = cohortDatabaseSchema,
+                                               cohortStagingTable = cohortTable,
+                                               cohortTable = cohortTable,
+                                               regimenIngredientsTable = "regimeningredienttable_voc",
+                                               createRegimenStats = F,
+                                               gapBetweenTreatment=120,
+                                               regimenStatsTable = 'test_rst',
+                                               exportFolder = outputFolder,
+                                               databaseId=databaseId,
+                                               databaseName=databaseName,
+                                               dropRegimenStatsTable = FALSE, # optional - drop created table
+                                               databaseDescription = ""
+  ), NA)
+
+})
+
