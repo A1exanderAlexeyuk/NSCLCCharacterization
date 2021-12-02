c037569400b849854a6e0e59027f9c7b260b1459
alexander alexeuyk <87428750+A1exanderAlexeyuk@users.noreply.github.com>
2021-12-02 14:16:52 +0300

    add tail with export func

--- R/CohortDiagnostics.R
+++ R/CohortDiagnostics.R
@@ -170,3 +170,175 @@ bundleDiagnosticsResults <- function(diagnosticOutputFolder, databaseId) {
   DatabaseConnector::createZipFile(zipFile = zipName, files = files)
   return(zipName)
 }
+
+#' @export
+exportResults <- function(exportFolder,
+                          databaseId,
+                          cohortIdsToExcludeFromResultsExport = NULL) {
+  filesWithCohortIds <- c("covariate_value.csv", "cohort_count.csv")
+  tempFolder <- NULL
+  ParallelLogger::logInfo("Adding results to zip file")
+  if (!is.null(cohortIdsToExcludeFromResultsExport)) {
+    ParallelLogger::logInfo("Exclude cohort ids: ", paste(cohortIdsToExcludeFromResultsExport, collapse = ", "))
+    # Copy files to temp location to remove the cohorts to remove
+    tempFolder <- file.path(exportFolder, "temp")
+    files <- list.files(exportFolder, pattern = ".*\\.csv$")
+    if (!file.exists(tempFolder)) {
+      dir.create(tempFolder)
+    }
+    file.copy(file.path(exportFolder, files), tempFolder)
+
+    # Censor out the cohorts based on the IDs passed in
+    for (i in 1:length(filesWithCohortIds)) {
+      fileName <- file.path(tempFolder, filesWithCohortIds[i])
+      fileContents <- readr::read_csv(fileName, col_types = readr::cols())
+      fileContents <- fileContents[!(fileContents$cohort_id %in% cohortIdsToExcludeFromResultsExport), ]
+      readr::write_csv(fileContents, fileName)
+    }
+
+    # Zip the results and copy to the main export folder
+    zipName <- zipResults(tempFolder, databaseId)
+    file.copy(zipName, exportFolder)
+    unlink(tempFolder, recursive = TRUE)
+    zipName <- file.path(exportFolder, basename(zipName))
+  } else {
+    zipName <- zipResults(exportFolder, databaseId)
+  }
+  ParallelLogger::logInfo("Results are ready for sharing at:", zipName)
+}
+
+zipResults <- function(exportFolder, databaseId) {
+  date <- format(Sys.time(), "%Y%m%dT%H%M%S")
+  zipName <- file.path(exportFolder, paste0("Results_", databaseId, "_", date, ".zip"))
+  files <- list.files(exportFolder, ".*\\.csv$")
+  oldWd <- setwd(exportFolder)
+  on.exit(setwd(oldWd), add = TRUE)
+  DatabaseConnector::createZipFile(zipFile = zipName, files = files)
+  return(zipName)
+}
+
+# Per protocol, we will only characterize cohorts with
+# >= 140 subjects to improve efficency
+getMinimumSubjectCountForCharacterization <- function() {
+  return(140)
+}
+
+getVocabularyInfo <- function(connection, cdmDatabaseSchema, tempEmulationSchema) {
+  sql <- "SELECT vocabulary_version FROM @cdmDatabaseSchema.vocabulary WHERE vocabulary_id = 'None';"
+  sql <- SqlRender::render(sql, cdmDatabaseSchema = cdmDatabaseSchema)
+  sql <- SqlRender::translate(sql, targetDialect = attr(connection, "dbms"), tempEmulationSchema = tempEmulationSchema)
+  vocabInfo <- DatabaseConnector::querySql(connection, sql)
+  return(vocabInfo[[1]])
+}
+
+#' @export
+getUserSelectableCohortGroups <- function() {
+  cohortGroups <- getCohortGroups()
+  return(unlist(cohortGroups[
+    cohortGroups$userCanSelect == TRUE,
+    c("cohortGroup")
+  ], use.names = FALSE))
+}
+
+formatCovariates <- function(data) {
+  # Drop covariates with mean = 0 after rounding to 4 digits:
+  if (nrow(data) > 0) {
+    data <- data[round(data$mean, 4) != 0, ]
+    covariates <- unique(data.table::setDT(data[, c("covariateId", "covariateName", "analysisId")]))
+    colnames(covariates)[[3]] <- "covariateAnalysisId"
+  } else {
+    covariates <- list("covariateId" = "", "covariateName" = "", "covariateAnalysisId" = "")
+  }
+  return(covariates)
+}
+
+
+loadCohortsFromPackage <- function(cohortIds) {
+  packageName <- getThisPackageName()
+  cohorts <- getCohortsToCreate()
+  cohorts <- cohorts %>% dplyr::mutate(atlasId = NULL)
+  if (!is.null(cohortIds)) {
+    cohorts <- cohorts[cohorts$cohortId %in% cohortIds, ]
+  }
+  if ("atlasName" %in% colnames(cohorts)) {
+    # Remove LungCancer cohort identifier (3.g. [LungCancer T1] )
+    cohorts <- cohorts %>%
+      dplyr::mutate(
+        cohortName = trimws(gsub("(\\[.+?\\])", "", atlasName)),
+        cohortFullName = atlasName
+      ) %>%
+      dplyr::select(-atlasName, -name)
+  } else {
+    cohorts <- cohorts %>% dplyr::rename(cohortName = name, cohortFullName = fullName)
+  }
+
+  getSql <- function(name) {
+    pathToSql <- system.file("sql", "sql_server", paste0(name, ".sql"),
+                             package = packageName, mustWork = TRUE
+    )
+    sql <- readChar(pathToSql, file.info(pathToSql)$size)
+    return(sql)
+  }
+  cohorts$sql <- sapply(cohorts$cohortId, getSql)
+  getJson <- function(name) {
+    pathToJson <- system.file("cohorts", paste0(name, ".json"),
+                              package = packageName,
+                              mustWork = TRUE
+    )
+    json <- readChar(pathToJson, file.info(pathToJson)$size)
+    return(json)
+  }
+  cohorts$json <- sapply(cohorts$cohortId, getJson)
+  return(cohorts)
+}
+
+loadCohortsForExportFromPackage <- function(cohortIds) {
+  packageName <- getThisPackageName()
+  cohorts <- getCohortsToCreate()
+  cohorts <- cohorts %>% dplyr::mutate(atlasId = NULL)
+  if ("atlasName" %in% colnames(cohorts)) {
+    # Remove LungCancer cohort identifier (3.g. [LungCancer O2])
+    # Remove atlasName and name from object to prevent clashes when combining with stratXref
+    cohorts <- cohorts %>%
+      dplyr::mutate(
+        cohortName = trimws(gsub("(\\[.+?\\])", "", atlasName)),
+        cohortFullName = atlasName
+      ) %>%
+      dplyr::select(-atlasName, -name)
+  } else {
+    cohorts <- cohorts %>% dplyr::rename(
+      cohortName = name,
+      cohortFullName = fullName
+    )
+  }
+}
+
+loadCohortsForExportWithChecksumFromPackage <- function(cohortIds) {
+  packageName <- getThisPackageName()
+  cohorts <- loadCohortsForExportFromPackage(cohortIds)
+}
+
+
+
+
+enforceMinCellValue <- function(data, fieldName, minValues, silent = FALSE) {
+  toCensor <- !is.na(data[, fieldName]) & data[, fieldName] < minValues & data[, fieldName] != 0
+  if (!silent) {
+    percent <- round(100 * sum(toCensor) / nrow(data), 1)
+    ParallelLogger::logInfo(
+      "   censoring ",
+      sum(toCensor),
+      " values (",
+      percent,
+      "%) from ",
+      fieldName,
+      " because value below minimum"
+    )
+  }
+  if (length(minValues) == 1) {
+    data[toCensor, fieldName] <- -minValues
+  } else {
+    data[toCensor, fieldName] <- -minValues[toCensor]
+  }
+  return(data)
+}
